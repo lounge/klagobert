82cea1940cae78b99bb3a051d618c8949c7958c7
Fred Lundén <fredlunden@gmail.com>
2016-04-28 00:12:07 +0200

    added logic for db, scoreboard, commands and sentiment parsing

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ var Klagobert = require('./lib/klagobert');
 
 var token = process.env.BOT_API_KEY;
 var name = process.env.BOT_NAME;
+var dbPath = process.env.MONGODB_URI;
 
 var klagobert = new Klagobert({
   token: token,
-  name: name
+  name: name,
+  dbPath: dbPath
 });
 
 klagobert.run();
