7a7f3ae072b96da8845f997c4f58318662882a96
Fred Lundén <fredlunden@gmail.com>
2016-04-28 00:14:56 +0200

    added channel env_var

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ var Klagobert = require('./lib/klagobert');
 var token = process.env.BOT_API_KEY;
 var name = process.env.BOT_NAME;
 var dbPath = process.env.MONGODB_URI;
+var channelId = process.env.CHANNEL_ID;
 
 var klagobert = new Klagobert({
   token: token,
   name: name,
-  dbPath: dbPath
+  dbPath: dbPath,
+  channelId: channelId
 });
 
 klagobert.run();
