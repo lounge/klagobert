9e1f3aabe61bde9d5eba15df2b041715dce169ad
Fred Lundén <fredlunden@gmail.com>
2016-05-07 16:40:51 +0200

    added express server/routes

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 'use strict'
 
 var Klagobert = require('./lib/klagobert');
+var Db = require('./lib/db');
+var Commands = require('./lib/commands');
+var Messages = require('./lib/messages');
+var Sentiment = require('./lib/sentiment');
+
+
+var express = require('express');
+var server = express();
 
 var token = process.env.BOT_API_KEY;
 var name = process.env.BOT_NAME;
 var dbPath = process.env.MONGODB_URI;
 var channelId = process.env.CHANNEL_ID;
 
+var db = new Db(dbPath);
+var messages = new Messages(db);
+var commands = new Commands(db, messages);
+var sentiment = new Sentiment();
+
 var klagobert = new Klagobert({
+  channelId: channelId,
   token: token,
   name: name,
-  dbPath: dbPath,
-  channelId: channelId
+  db: db,
+  messages: messages,
+  commands: commands,
+  sentiment: sentiment
 });
+
+var routes = require('./lib/routes')(klagobert);
+
+
+
+// routes.bindKlagobert(klagobert);
+
+server.use('/', routes);
+server.listen(3000);
 
 klagobert.run();
